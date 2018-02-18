2920b0e1f434dc28e09fea506d9897c7889fd24f
Adam Dyson <contact@adamdyson.com.au>
2018-02-19 04:55:14 +1100

    Verify service names and postgresql version

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # Verify the installed PostgreSQL version.
-docker exec --tty ${container_id} env TERM=xterm initctl list
-
-# Verify the installed PostgreSQL version.
 docker exec --tty ${container_id} env TERM=xterm which psql -V
