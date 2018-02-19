ef5c86de86b812abb6a65e9da9c1021cf237bb8f
Adam Dyson <contact@adamdyson.com.au>
2018-02-19 16:37:43 +1100

    Updated tests

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,5 +2,4 @@
 
 # Verify the installed PostgreSQL version.
 docker exec --tty ${container_id} env TERM=xterm psql -V
-docker exec --tty ${container_id} env TERM=xterm journalctl -xe || true
 docker exec --tty ${container_id} env TERM=xterm ls /etc/init.d/ || true
