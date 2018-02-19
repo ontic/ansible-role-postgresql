bb7b0d070d4d00ee672f68f8a1be9bd86a1eaba3
Adam Dyson <contact@adamdyson.com.au>
2018-02-20 04:37:46 +1100

    Like the ontic.mysql role, we will override the global configuration file or create it for the first time, based on a ninja template, thereafter settings will be defined via ini_file module. A reason for doing things this way is partly because of this bug: https://github.com/ansible/ansible/issues/30599#issuecomment-348729067

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -5,7 +5,8 @@ docker exec --tty ${container_id} env TERM=xterm psql -V
 
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
     docker exec --tty ${container_id} env TERM=xterm ls /var/lib/postgresql/10/main
-    docker exec --tty ${container_id} env TERM=xterm cat /var/lib/postgresql/10/main/postgresql.conf
+    docker exec --tty ${container_id} env TERM=xterm ls /etc/postgresql/10/main
+    docker exec --tty ${container_id} env TERM=xterm cat /etc/postgresql/10/main/postgresql.conf
 elif [ "${distribution}" = "centos" ]; then
     docker exec --tty ${container_id} env TERM=xterm ls /var/lib/pgsql/10/data
     docker exec --tty ${container_id} env TERM=xterm cat /var/lib/pgsql/10/data/postgresql.conf
