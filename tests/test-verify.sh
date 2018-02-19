e6b14e157638bcc7b099143e36527b11f6502572
Adam Dyson <contact@adamdyson.com.au>
2018-02-19 20:37:33 +1100

    Removed the task which sets environment variables, as I'm not convinced it's needed. Added output of certain directories

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,3 +2,10 @@
 
 # Verify the installed PostgreSQL version.
 docker exec --tty ${container_id} env TERM=xterm psql -V
+
+if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
+    docker exec --tty ${container_id} env TERM=xterm ls /usr/lib/postgresql/10
+elif [ "${distribution}" = "centos" ]; then
+    docker exec --tty ${container_id} env TERM=xterm ls /var/lib/pgsql/10
+    docker exec --tty ${container_id} env TERM=xterm ls /usr/pgsql-10
+fi
