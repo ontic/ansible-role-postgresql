e690ae624f6d668888e231035c6dc3e4dfb4010d
Adam Dyson <contact@adamdyson.com.au>
2018-02-19 20:52:08 +1100

    Peek at the data directory contents

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -4,7 +4,7 @@
 docker exec --tty ${container_id} env TERM=xterm psql -V
 
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
-    docker exec --tty ${container_id} env TERM=xterm ls /usr/lib/postgresql/10/main
+    docker exec --tty ${container_id} env TERM=xterm ls /var/lib/postgresql/10
 elif [ "${distribution}" = "centos" ]; then
     docker exec --tty ${container_id} env TERM=xterm ls /var/lib/pgsql/10/data
 fi
