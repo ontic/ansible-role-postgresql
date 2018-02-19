71fcbfa5475e765deff1561f2d418867236477ae
Adam Dyson <contact@adamdyson.com.au>
2018-02-19 16:13:56 +1100

    Take a peek at journalctl to see why Centos cannot start postgresql

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,4 +2,5 @@
 
 # Verify the installed PostgreSQL version.
 docker exec --tty ${container_id} env TERM=xterm psql -V
+docker exec --tty ${container_id} env TERM=xterm journalctl -xe || true
 docker exec --tty ${container_id} env TERM=xterm ls /etc/init.d/ || true
