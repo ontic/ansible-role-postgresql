fd2a5e5a626c00f8f8b337decc46f8ecaef4a04f
Adam Dyson <contact@adamdyson.com.au>
2018-02-20 12:39:30 +1100

    Debug folder locations on CentOS

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -4,10 +4,13 @@
 docker exec --tty ${container_id} env TERM=xterm psql -V
 
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
-    docker exec --tty ${container_id} env TERM=xterm ls /var/lib/postgresql/10/main && echo "\n\n"
-    docker exec --tty ${container_id} env TERM=xterm ls /etc/postgresql/10/main && echo "\n\n"
-    docker exec --tty ${container_id} env TERM=xterm cat /etc/postgresql/10/main/postgresql.conf && echo "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /var/lib/postgresql/10/main && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /etc/postgresql/10/main && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm cat /etc/postgresql/10/main/postgresql.conf && echo -e "\n\n"
 elif [ "${distribution}" = "centos" ]; then
-    docker exec --tty ${container_id} env TERM=xterm ls /var/lib/pgsql/10/data && echo "\n\n"
-    docker exec --tty ${container_id} env TERM=xterm cat /var/lib/pgsql/10/data/postgresql.conf && echo "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /var/lib/pgsql/10/data && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /etc/ && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /etc/postgresql/10 && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /etc/postgresql/10/main && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm cat /var/lib/pgsql/10/data/postgresql.conf && echo -e "\n\n"
 fi
