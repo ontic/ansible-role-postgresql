4a46bfafb95e4d43518d23ab7e89e6c9babfb290
Adam Dyson <contact@adamdyson.com.au>
2018-02-28 03:03:19 +1100

    Debug

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -4,7 +4,7 @@
 docker exec --tty ${container_id} env TERM=xterm psql -V
 
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
-    docker exec --tty ${container_id} env TERM=xterm journalctl --no-pager
+    docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql.service --no-pager
 elif [ "${distribution}" = "centos" ]; then
-    docker exec --tty ${container_id} env TERM=xterm journalctl --no-pager
+    docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql-10.service --no-pager
 fi
