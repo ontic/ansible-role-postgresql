c6d40f6b2abc41ff33dbdc21bf3c4103fd7e7edd
Adam Dyson <contact@adamdyson.com.au>
2018-02-28 02:36:44 +1100

    Debug

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -6,5 +6,5 @@ docker exec --tty ${container_id} env TERM=xterm psql -V
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
     docker exec --tty ${container_id} env TERM=xterm service postgresql status
 elif [ "${distribution}" = "centos" ]; then
-    docker exec --tty ${container_id} env TERM=xterm ls service postgresql-10 status
+    docker exec --tty ${container_id} env TERM=xterm service postgresql-10 status
 fi
