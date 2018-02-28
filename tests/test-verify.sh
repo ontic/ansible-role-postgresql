5e5ea77058e16452a04068f4da4d8e351ac9fb6d
Adam Dyson <contact@adamdyson.com.au>
2018-02-28 20:53:47 +1100

    Removed command added when debugging

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,11 +2,3 @@
 
 # Verify the installed PostgreSQL version.
 docker exec --tty ${container_id} env TERM=xterm psql -V
-
-if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
-    docker exec --tty ${container_id} env TERM=xterm cat /etc/postgresql/10/main/postgresql.conf && echo -e "\n\n"
-    docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql.service --no-pager
-elif [ "${distribution}" = "centos" ]; then
-    docker exec --tty ${container_id} env TERM=xterm cat /var/lib/pgsql/10/data/postgresql.conf && echo -e "\n\n"
-    docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql-10.service --no-pager
-fi
