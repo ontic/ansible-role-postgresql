6a4291e66e9958892ba70b99ab4fd90d0315ffb6
Adam Dyson <contact@adamdyson.com.au>
2018-02-28 19:51:12 +1100

    Debug the contents of the config file, which I can compare with the default configuration which works

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -4,11 +4,9 @@
 docker exec --tty ${container_id} env TERM=xterm psql -V
 
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
-    docker exec --tty ${container_id} env TERM=xterm ls /var/run/postgresql && echo -e "\n\n"
-    docker exec --tty ${container_id} env TERM=xterm ls /tmp && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm cat /etc/postgresql/10/main/postgresql.conf && echo -e "\n\n"
     docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql.service --no-pager
 elif [ "${distribution}" = "centos" ]; then
-    docker exec --tty ${container_id} env TERM=xterm ls /var/run/postgresql && echo -e "\n\n"
-    docker exec --tty ${container_id} env TERM=xterm ls /tmp && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm cat /var/lib/pgsql/10/data/postgresql.conf && echo -e "\n\n"
     docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql-10.service --no-pager
 fi
