502c1555f5be6dd8e68b65c8dcf7da857ba2e2a4
Adam Dyson <contact@adamdyson.com.au>
2018-02-28 18:06:34 +1100

    Still convinced the socket file is located in the wrong directory

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -4,7 +4,11 @@
 docker exec --tty ${container_id} env TERM=xterm psql -V
 
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
+    docker exec --tty ${container_id} env TERM=xterm ls /var/run/postgresql && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /tmp && echo -e "\n\n"
     docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql.service --no-pager
 elif [ "${distribution}" = "centos" ]; then
+    docker exec --tty ${container_id} env TERM=xterm ls /var/run/postgresql && echo -e "\n\n"
+    docker exec --tty ${container_id} env TERM=xterm ls /tmp && echo -e "\n\n"
     docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql-10.service --no-pager
 fi
