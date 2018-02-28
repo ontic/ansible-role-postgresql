281e71598f3ad1cdffa7fb22f324695441d0bc4b
Adam Dyson <contact@adamdyson.com.au>
2018-02-28 17:42:50 +1100

    Define listening address in template

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -4,9 +4,7 @@
 docker exec --tty ${container_id} env TERM=xterm psql -V
 
 if [ "${distribution}" = "debian" ] || [ "${distribution}" = "ubuntu" ]; then
-    docker exec --tty ${container_id} env TERM=xterm netstat -lp --protocol=unix | grep postgres
     docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql.service --no-pager
 elif [ "${distribution}" = "centos" ]; then
-    docker exec --tty ${container_id} env TERM=xterm netstat -lp --protocol=unix | grep postgres
     docker exec --tty ${container_id} env TERM=xterm journalctl -u postgresql-10.service --no-pager
 fi
