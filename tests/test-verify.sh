1afa33d84f8ac55f40159b5c3a95379fba3f779b
Adam Dyson <contact@adamdyson.com.au>
2018-03-03 04:57:27 +1100

    Tests are no passing, so hiding passwords from logged output has been enabled again. Verify tests no lists all databases

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,3 +2,6 @@
 
 # Verify the installed PostgreSQL version.
 docker exec --tty ${container_id} env TERM=xterm psql -V
+
+# Verify that databases were created.
+docker exec --tty ${container_id} env TERM=xterm psql -l
