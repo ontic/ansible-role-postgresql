a51c6a2889aca9a4bd31170a652aa0d22b136f20
Adam Dyson <contact@adamdyson.com.au>
2018-03-03 05:23:04 +1100

    Switched to postgres user when executing command

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -4,4 +4,4 @@
 docker exec --tty ${container_id} env TERM=xterm psql -V
 
 # Verify that databases were created.
-docker exec --tty ${container_id} env TERM=xterm psql -l -U postgres
+docker exec --tty ${container_id} env TERM=xterm sudo -u postgres psql -l -U postgres
