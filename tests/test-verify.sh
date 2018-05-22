c81b98144ef6252d96cf05077742da3f9d324793
Adam Dyson <adam.dyson@outlook.com>
2018-05-23 01:28:03 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed PostgreSQL version.
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which psql
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm psql -V
-
 # Verify that databases were created.
 docker exec --tty ${container_id} env TERM=xterm sudo -u postgres psql -l -U postgres
